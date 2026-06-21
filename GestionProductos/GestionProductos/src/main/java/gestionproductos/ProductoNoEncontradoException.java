c951c54073ea8787eb22e8e4a0ed48bfffded5e8
Paulo Barrantes <paulocbm05@gmail.com>
2026-06-21 15:01:17 -0600

    Delete GestionProductos/GestionProductos directory

--- GestionProductos/GestionProductos/src/main/java/gestionproductos/ProductoNoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package gestionproductos;
-
-  //aparece cuando se busca un producto por código y no existe.
-public class ProductoNoEncontradoException extends Exception {
-
-    public ProductoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
